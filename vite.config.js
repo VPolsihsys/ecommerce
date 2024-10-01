9248fbe2fb54234bb7782f6af70a9eae8c4b4af1
VPolsihsys <38548075+VPolsihsys@users.noreply.github.com>
2024-10-01 13:17:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/workspaces/ecommerce/index.html',  // Ensure this points to your actual HTML file
-    },
-  },
 });
