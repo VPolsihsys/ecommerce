667ef8848cdbd6d3f5bbcf1085f4bc91e83b06d7
VPolsihsys <38548075+VPolsihsys@users.noreply.github.com>
2024-10-01 13:09:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/ecommerce/',  // Add this line
+  root: './src', // Change this if your index.html is in a different folder
   plugins: [react()],
 });
+
